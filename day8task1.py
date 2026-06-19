bab66f38087311bd4f681a21cd00fd04ecb00b7a
manimala651 <manimala0613@gmail.com>
2026-06-19 18:06:16 +0530

    Add files via upload

---
+++ day8task1.py
@@ -0,0 +1,31 @@
+print('---Task 1---')
+
+tup_color=('Blue','Green','Yellow','pink','Red')
+
+print('to print the second element')
+print(tup_color[1])
+
+print('Length of the tuple')
+print(len(tup_color))
+
+print('Multiple operator')
+print(tup_color*3)
+
+
+print('red' in tup_color)
+print('silver' in tup_color)
+print('Red' in tup_color)
+
+
+print('---Task 2---')
+emp1=('john',30,'sales department',50000)
+emp2=('hari',23,'marketing department',48000)
+print("employee 1 details:",emp1)
+print("employee 2 details:",emp2)
+
+print("department:",emp1[2])
+
+
+print("Average age of the two employee")
+avg=(emp1[1]+emp2[1])/2
+print("average age :",avg)
